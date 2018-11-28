b77d5d9afd845d11f627fc7118a1774a03e1c80b
Sinlory <1152657503@qq.com>
2018-11-28 19:58:03 +0800

    增加ref和params参数的练习。

---
+++ LearnTest03/02ref参数/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02ref参数
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double salary = 5000;
+            JiangLi(ref salary);
+            Console.WriteLine(salary);
+            Console.ReadKey();
+        }
+
+        public static void JiangLi(ref double s)
+        {
+            s += 500;
+        }
+    }
+}
---
+++ LearnTest03/03params可变参数/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _03params可变参数
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = { 23, 45, 44 };
+            //Test(23, 45, 67);//可接逗号分隔的指定类型的数值
+            //Test();//可什么都不接
+            Test(nums);//可接指定类型的数组
+        }
+
+        public static void Test(params int[] nums)
+        {
+            int sum = 0;
+            for (int i = 0; i < nums.Length; i++)
+            {
+                sum += nums[i];
+            }
+            Console.WriteLine(sum);
+            Console.ReadKey();
+        }
+    }
+}
