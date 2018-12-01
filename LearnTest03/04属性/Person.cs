fb9b3d68ce728acf594b66405eb5301bf63424a8
Sinlory <1152657503@qq.com>
2018-12-01 21:11:15 +0800

    增加属性的练习

---
+++ LearnTest03/04属性/Person.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _04属性
+{
+    public class Person
+    {
+        private int _age;
+        public int Age
+        {
+            set { _age = value; }
+            get { return _age; }
+            //可以在{}中增加限定条件
+        }
+
+        public static void Main(string[] args)
+        {
+
+        }
+    }
+
+}
